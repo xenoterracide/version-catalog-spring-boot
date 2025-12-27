ffd03c78c3e4b7d11547535bbb7731abf4bf9188
Caleb Cushing <caleb.cushing@gmail.com>
2025-12-27 16:37:23 -0500

    chore: remove unused `stagingPath` task, update `publish` plugin to v0.4.+, and fix typo in `websiteUrl` configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,25 +40,6 @@ tasks.register("version") {
   actions.add { println(setVersion.toString()) }
 }
 
-tasks.register("stagingPath") {
-  description = "Print path to staging repository for the primary publication"
-  group = "Publishing"
-  val stagingDir = stagingPath
-  val groupPath = project.group.toString().replace(".", "/")
-  val artifactId = project.name
-  val setVersion = version
-
-  actions.add {
-    println(
-      stagingDir
-        .get()
-        .asFile
-        .resolve("$groupPath/$artifactId/$setVersion")
-        .absolutePath,
-    )
-  }
-}
-
 repositoryHost(GithubPublicRepositoryConfiguration())
 repositoryHost.namespace.set("xenoterracide")
 
@@ -74,7 +55,7 @@ publishing {
       pom {
         description.set("Version catalog for Spring Boot dependencies")
         url.set(
-          repositoryHost.repository.wesiteUrl
+          repositoryHost.repository.websiteUrl
             .zip(git.tag.map { "/tree/$it" }.orElse("")) { uri, tag ->
               uri.toString() + tag
             },
